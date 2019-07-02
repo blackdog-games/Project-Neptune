3067875bc5f3dd88d100c4aa5f08e589bb263b94
Mykolas Kunevičius <kunevicius.mykolas@gmail.com>
2019-07-02 21:53:21 +0300

    More experimentation

---
+++ Project Neptune/Assets/simplemove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simplemove : MonoBehaviour
+{
+    public Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKey(KeyCode.Space))
+        //{
+            RB.AddForce(Vector3.forward*99);
+        //}
+    }
+}
